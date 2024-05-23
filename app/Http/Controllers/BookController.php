fb593707cbeea6b71e10764a84b642947d6ea12a
Umer Saeed <umer2saeed@gmail.com>
2024-05-24 01:15:33 +0500

    working on book model

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        return view('books.list');
+    }
+
+    public function create()
+    {
+        return view('books.create');
+    }
+
+    public function store(Request $request)
+    {
+        $rules = [
+            'title' => 'required|min:5',
+            'author' => 'required|min:3',
+            'status' => 'required'
+        ];
+
+        if (!empty($request->image)) {
+            $rules['image'] = 'image';
+        }
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return redirect()->route('books.create')->withInput()->withErrors($validator);
+        }
+
+        $book = new Book();
+        $book->title = $request->title;
+        $book->author = $request->author;
+        $book->description = $request->description;
+        $book->status = $request->status;
+        $book->save();
+
+        return redirect()->route('books.index')->with('success', 'Book added successfully!');
+    }
+
+    public function edit()
+    {
+//        return view('books.edit');
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+
+}
